99a55b8644ab9c0c7ca81b7ca92141f839fd46ce
ThiagoLp <thiagoluiz.ps@hotmail.com>
2022-03-20 16:00:51 -0300

    chore: add utils pixelTrem and change Header sizes

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,18 +1,24 @@
 import styled from "styled-components";
+import { pixelToRem } from '../../utils/pixeltorem'
 
 export const Container = styled.header`
   max-width: calc(1091px + 2rem);
+  min-width: 360px;
   padding: 0 2rem;
   margin: 0 auto;
-  margin-top: 41px;
+  margin-top: ${pixelToRem(41)};
 
   display: flex;
   justify-content: space-between;
   align-items: center;
 
   img {
-    width: 100px;
-    height: 48.11px;
+    width: ${pixelToRem(100)};
+    height: ${pixelToRem(48.11)};
+  }
+
+  @media screen and (max-width: 480px){
+    margin-top: ${pixelToRem(14)};
   }
 `
 
@@ -22,18 +28,13 @@ export const SocialLinks = styled.nav`
   align-items: center;
 
   a {
-    /* padding: 13px; */
-
     & + a {
-      margin-left: 28px;
+      margin-left: ${pixelToRem(28)};
     }
 
     img {
-      width: 24px;
-      height: 24px;
+      width: ${pixelToRem(24)};
+      height: ${pixelToRem(24)};
     }
   }
-
-
-
 `
